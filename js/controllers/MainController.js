2b692f14b125adff7a2bff2cfe72989576ac6fbb
dchen71 <dchen12812@gmail.com>
2015-07-25 15:24:24 -0700

    implemented solution to repeat menu items

--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -11,7 +11,48 @@ app.controller('MainController', ['$scope', function($scope) {
       name: 'Mozzarella Sticks',
       description: 'Served with marinara sauce.',
       price: 3.95
-    }
+    },
+    {
+      name: 'Bruschetta',
+      description: 'Grilled bread garlic, tomatoes, olive oil',
+      price: 4.95
+    }    
   ];
 
+  $scope.mains = [
+    {
+      name: 'Steak',
+      description: 'Balsmaic glaze.',
+      price: 14.95
+    },
+    {
+      name: 'Mozzarella Pasta',
+      description: 'Served with marinara sauce.',
+      price: 13.95
+    },
+    {
+      name: 'Salmon Steak',
+      description: 'Grilled garlic, tomatoes, olive oil',
+      price: 24.95
+    }    
+  ];
+  
+  $scope.extras = [
+    {
+      name: 'Ice Cream',
+      description: 'Mozzarella flavor',
+      price: 2.95
+    },
+    {
+      name: 'Cake',
+      description: 'Served with marinara sauce.',
+      price: 2.95
+    },
+    {
+      name: 'Cheesecake',
+      description: 'Olive oil',
+      price: 4.95
+    }    
+  ];
+  
 }]);
